1375cb34fac45f8ea41556e9a628df1744079f8b
Vagabov Sergey <xfter16@gmail.com>
2017-09-22 16:42:07 +0300

    add cool-ascii-faces

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 var express = require('express');
+var cool = require('cool-ascii-faces');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(request, response) {
   response.end('hello world!');
+
+});
+app.get('/cool', function(req, res){
+	res.send(cool());
 });
 
 app.listen(app.get('port'), function() {
